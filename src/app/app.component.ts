810c88eabb2a7617f523a2be2c2dc16488e6ef78
Michał Paduch (INI) <INI@kmd.dk>
2023-02-15 14:36:36 +0100

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
+import { AnalyticsService } from './@core/utils/analytics.service';
+import { SeoService } from './@core/utils/seo.service';
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
+
+  constructor(
+    private analytics: AnalyticsService,
+    private seoService: SeoService,
+    private themeService: NbThemeService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.analytics.trackPageViews();
+    this.seoService.trackCanonicalChanges();
+    this.themeService.changeTheme('dark');
+  }
+}
